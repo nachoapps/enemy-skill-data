8f36a08e614c3a88b2d21f37dd21fda93ff487d6
nachoapps <nachoapps@gmail.com>
2019-05-27 22:38:46 -0400

    Initial commit of es endpoint

---
+++ web/get_es_info.php
@@ -0,0 +1,35 @@
+<?php
+
+$monster_id = "";
+if (array_key_exists("monster_id", $_GET)) {
+    $monster_param = $_GET["monster_id"];
+    if (is_numeric($monster_param)) {
+        $monster_id = $monster_param;
+    }
+}
+
+if ($monster_id == "") {
+  die("You must supply an integer monster_id");
+}
+
+$data_home = "/home/tactical0retreat/pad_data";
+$es_home = "/home/tactical0retreat/git/enemy-skill-data";
+$api_home = "/home/tactical0retreat/rpad-cogs-utils/pad_api_data";
+
+$raw_dir = $data_home . "/raw";
+$processed_dir = $data_home . "/processed";
+$es_dir = $es_home . "/enemy_data";
+
+
+$encoding = "export PYTHONIOENCODING=utf-8;"
+$pypath = "export PYTHONPATH=" . $api_home . ";"
+$script = "print_dungeon.py";
+
+$cmd = "python3 " . $script
+    . " --monster_id=" . $monster_id
+    . " --es_input_dir=" . $es_dir;
+
+$final_cmd = $encoding . $pypath . $cmd;
+passthru($final_cmd, $err);
+
+?>
---
+++ web/get_es_info.py
@@ -0,0 +1,53 @@
+"""`
+
+"""
+
+import argparse
+import logging
+import yaml
+import json
+
+
+from pad_etl.processor import enemy_skillset_dump as esd
+
+fail_logger = logging.getLogger('processor_failures')
+fail_logger.disabled = True
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="Dumps data.", add_help=False)
+    inputGroup = parser.add_argument_group("Input")
+    inputGroup.add_argument("--monster_id", required=True, help="Monster ID")
+    inputGroup.add_argument("--es_input_dir", required=True,
+                            help="Path to a folder where the enemy skills data is")
+
+    helpGroup = parser.add_argument_group("Help")
+    helpGroup.add_argument("-h", "--help", action="help",
+                           help="Displays this help message and exits.")
+    return parser.parse_args()
+
+
+def run_dump(args):
+    esd.set_data_dir(args.es_input_dir)
+
+    summary = esd.load_summary(args.monster_id)
+    info = yaml.dump(summary.info, default_flow_style=False, allow_unicode=True)
+    levels = {}
+
+    for listing in summary.data:
+        level_data = {
+            'raw': yaml.dump(listing, default_flow_style=False, allow_unicode=True),
+            'processed': esd.summary_as_dump_text(summary, listing.level, 1),
+        }
+        levels[listing.level] = level_data
+
+    output = {
+        'info': info,
+        'levels': levels,
+    }
+
+    print(json.dumps(output, indent=2, sort_keys=True))
+
+
+if __name__ == '__main__':
+    args = parse_args()
+    run_dump(args)
